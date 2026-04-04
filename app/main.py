989c00b5de3a73e2dd37c560d083c97cb4f4ce2e
knowmaker <59835668+knowmaker@users.noreply.github.com>
2026-04-04 22:26:44 +0300

    added CORS

--- app/main.py
+++ app/main.py
@@ -7,6 +7,18 @@ from app.core.config import settings
 app = FastAPI(title=settings.APP_NAME)
 app.include_router(api_router, prefix=settings.API_V1_PREFIX)
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          # или ["*"] для полного разрешения (не советую для прод)
+    allow_credentials=True,         # нужно если используешь cookies/authorization
+    allow_methods=["*"],            # GET/POST/PATCH/DELETE...
+    allow_headers=["*"],            # Authorization, Content-Type и т.п.
+)
 
 @app.get("/health", tags=["health"])
 def health() -> dict[str, str]:
